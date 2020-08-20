6ecac87f36e1b7535cc83710d15a8731fe1560f5
Aitous <youssef.aitousarrah@gmail.com>
2020-08-20 01:07:29 -0700

    Updates

---
+++ precache-manifest.2b78189418bd6244885d8d2682980b52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770083ff028a7daef096e9768489385b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c19e67ae477834b43ed1",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e45d130daeeb80d116f4",
+    "url": "/home/static/css/main.6a3d9078.chunk.css"
+  },
+  {
+    "revision": "c19e67ae477834b43ed1",
+    "url": "/home/static/js/2.06c37fd8.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/home/static/js/2.06c37fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45d130daeeb80d116f4",
+    "url": "/home/static/js/main.522ff750.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "c65637e779fcefc7ef291bfdde9b82fb",
+    "url": "/home/static/media/Profile.c65637e7.JPG"
+  },
+  {
+    "revision": "2a1ad103715512cb66441801cc03a2ed",
+    "url": "/home/static/media/resume.2a1ad103.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30f72245d5af87d56c64251a34772388.js"
+  "/home/precache-manifest.2b78189418bd6244885d8d2682980b52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
